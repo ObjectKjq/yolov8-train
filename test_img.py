df05c5d71be813198f1aa22378a80d6dc128daf7
kongdefang <1208230746@qq.com>
2025-12-19 11:15:37 +0800

    初始化

---
+++ test_img.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+model = YOLO("./runs/detect/train/weights/best.pt")  # 加载模型
+print(f"检测类别: {model.names}")
+# 执行检测并保存结果（默认存到runs/detect/predict）
+model.predict(source='./images/2008_002378.jpg', save=True, conf=0.6)
